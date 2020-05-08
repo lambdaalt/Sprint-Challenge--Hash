5b324a123b28408ec79db4106347b2ba4b326d47
melissa-murphy <melissamurphy.ca@gmail.com>
2020-05-08 12:04:42 -0700

    Working!

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,22 @@
 def intersection(arrays):
-
-    """
-    YOUR CODE HERE
-    """
-
+    ht = {}
+    # removed result = [] and append, works now
+    
+    for arr in arrays:
+        for i in arr:
+            if i not in ht:
+                ht[i] = 1
+            else:
+                ht[i] += 1
+    result = [key for key in ht if ht[key] == len(arrays)]
     return result
 
 
 if __name__ == "__main__":
     arrays = []
 
-    arrays.append(list(range(1000000,2000000)) + [1,2,3])
-    arrays.append(list(range(2000000,3000000)) + [1,2,3])
-    arrays.append(list(range(3000000,4000000)) + [1,2,3])
+    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))

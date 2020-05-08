fe096f6e9a4951b34603aea2e95118e9ad1ebf6a
melissa-murphy <melissamurphy.ca@gmail.com>
2020-05-08 09:47:10 -0700

    notes

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,6 +1,6 @@
 def get_indices_of_item_weights(weights, length, limit):
     w = {}
-    
+    # would a keysfrom() be a better use here?
     for i in range(length):
         key = limit - weights[i]
         if key in w.values():
